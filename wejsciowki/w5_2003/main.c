913926ba8e655e309b08c5ca87b04db6f937c7f2
KrzysztofKrupicki <147048678+KrzysztofKrupicki@users.noreply.github.com>
2024-03-20 11:23:36 +0100

    dodano rozwiazanie wejsciowki nr5

---
+++ wejsciowki/w5_2003/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void substractAndStore(int substractor, int *numberPtr){
+    *numberPtr = *numberPtr - substractor;
+}
+
+int main()
+{
+    int a = 2;
+    int b = 7;
+    printf("\nPrzed: a = %d, b = %d", a, b);
+    substractAndStore(a, &b);
+    printf("\nPo: a = %d, b = %d", a, b);
+    return 0;
+}
